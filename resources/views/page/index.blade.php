5112d697c283dea86ef17cd142751e9a428933d9
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-05-19 22:55:39 +0700

    Create index view

---
+++ resources/views/page/index.blade.php
@@ -0,0 +1,82 @@
+@extends('page.layouts.master')
+
+@section('content')
+<section id="content">
+    <div class="container_12">  
+      <div class="grid_8">
+        <h2 class="top-1 p3">Welcome to The Villa Lombok!</h2>
+        <p class="p2"><p>Exploring Lombok</p>
+<p>
+
+<p>It's like everything has been saturated with an extra splash color in Lombok, an island of tranquility in the middle of the Indonesian archipelago. Lombok luxury villas capture the atmosphere to a T, with rich Asian décor and calm spaces, thoroughly modern though still old-world. Our collection of Lombok villa rentals are located on the south side of the island, where white-sand beaches line the shore and surroundings are a mix of tourist conveniences - like gourmet restaurants - and authentic Lomboknese experiences. Choose a Lombok vacation villa if you like warm, languid days and you're in the mood to slow down for a little while. </p>
+</p>
+        <p class="line-1">
+        <p>Experience Lombok villas</p>
+        <p>Guests of Lombok villa rentals can be forgiven for wanting to spend their days relaxing in the splendor of their private and luxurious estates. With spacious pools, lush tropical gardens and attentive service luxury Lombok villas offer guests a taste of paradise.
+
+For adventure and culture lovers however, Lombok offers so much more to discover. Venture inland to Ubud, Lombok's green, cultural heart or admire the temple of Uluwatu, perched on an oceanside cliff. Mingle with the crowds at a local market and pick up bargains on fabric and wood crafts made by Lomboknese artisans.
+
+Treck through the lush jungle on the back of an elephant or get a close up look at Lombok’s most colorful residents with a Lombok bird walk. Watersport lovers can take their pick – from diving and snorkeling to surfing some of Lombok’s most exciting activities can be found just offshore.
+
+For those seeking a relaxing escape from the stress of everyday life, Lombok luxury villas are a destination of choice. Ask your Dedicated Concierge about booking massage treatments at your Lombok villa rental or for recommendations on local yoga retreats. 
+</p></p>
+        <h2 class="p4">Buyers. Sellers. Agents.</h2>
+        <div class="wrap block-1">
+            <div>
+                <img src="images/page1-img2.jpg" alt="" class="img-border">
+                <h3>Book now</h3>
+                <p>Find your favorite villa now just by one click. We provide all villas in lombok with the perfect details. </p>
+                <a href="book now.html" class="button">Book now</a></p>
+            </div>
+            <div>
+                <img src="images/page1-img1.jpg" alt="" class="img-border">
+                <h3>Buying</h3>
+                <p>Are you looking for a beautiful sale villa in Lombok? We help you find the perfect villa for living. </p>
+                <a href="buying.html" class="button">Buy now</a>
+            </div>
+            <div class="last">
+                <img src="images/page1-img3.jpg" alt="" class="img-border">
+                <h3>Selling</h3>
+                <p>Sell your Property by online "Selling" service that lets you advertise any kind of villa for sale in Lombok on our website. </p>
+                <a href="#" class="button">Sign Up now</a>
+            </div>
+        </div>
+      </div>
+<!--==============================Right_Side==========================================================-->
+      <div class="grid_4">
+        <div class="left-1">
+            <h2 class="top-1 p3">Comfort villa for the best living</h2>
+            <form id="form-1" class="form-1 bot-1">
+                  <div>
+                <img src="images/page1-img5.jpg" alt="" class="img-border">
+                <h3>Breathtaking view</h3>
+                <p>You can choose your view for your living, such as ocean view, garden view, mountain and hills view, and field view.</p>
+                <a href="book now.html" class="button">Find now</a>
+            </div>
+                <div>
+                <img src="images/page1-img6.jpg" alt="" class="img-border">
+                <h3>Many facilities</h3>
+                <p>Need some standard facilities for your living? You can add them as you want!</p>
+                <a href="book now.html" class="button">Find now</a>
+            </div>
+            <div>
+                <img src="images/page1-img7.jpg" alt="" class="img-border">
+                <h3>Best price</h3>
+                <p>Living happily with no worry about the cost, cause we provide the best price based on your budget.</p>
+                <a href="book now.html" class="button">Find now</a>
+            </div>
+<!--==============================Add_Note==========================================================-->
+            </form>
+            <h2 class="p3">Find our office</h2>
+            <img src="images/page1-img4.png" alt="">
+           <dl>
+                <dt class="color-1 p2"><strong>Majapahit Road,<br>Mataram City.</strong></dt>
+                <dd><span>Telephone:</span>(0370) 636 001</dd>
+                <dd><span>E-mail:</span><a href="#" class="link">madinalombok@gmail.com</a></dd>
+            </dl>
+        </div>
+      </div>
+      <div class="clear"></div>
+    </div>  
+</section> 
+@endsection
