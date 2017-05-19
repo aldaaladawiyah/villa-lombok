9b654183028353c40e212656a7d39192c3aed59c
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-05-19 22:56:20 +0700

    Create contact us view

---
+++ resources/views/page/contact-us.blade.php
@@ -0,0 +1,72 @@
+@extends('page.layouts.master')
+
+@section('content')
+<section id="content">
+    <div class="container_12">  
+      <div class="grid_8">
+        <h2 class="top-1 p3">Romantic</h2>
+        <div class="wrap">
+            <img src="images/page5-img1.jpg" alt="" class="img-border img-indent">
+            <div class="extra-wrap">
+                <p class="color-1 p6">Aenean quis metus lacus, a commodo libero nam lacinia blandit dui vitae malesuada donec</p>
+                <p>Pellentesque scelerisque orci, ac tempor purus vulputate lobortis. Vestibulum porttitor sem mattis eros posuere vitae tristique justo congue curabitur consectetur.</p>
+            </div>
+        </div>
+        <p class="p5">Venenatis aliquam sit amet arcu justo in commodo consectetur lacus ac ultrices cras porta dignissim turpis fermentum porttitor aenean scelerisque nunc vel turpis faucibus vestibulum aenean volutpat iaculis nunc, sed accumsan lacus imperdiet eu.</p>
+        <a href="#" class="button">Read more</a>
+        <h2 class="top-2 p3">Party</h2>
+        <div class="wrap">
+            <img src="images/page5-img2.jpg" alt="" class="img-border img-indent">
+            <div class="extra-wrap">
+                <p class="color-1 p6">Aenean quis metus lacus, a commodo libero nam lacinia blandit dui vitae malesuada donec </p>
+                <p>Pellentesque scelerisque orci, ac tempor purus vulputate lobortis. Vestibulum porttitor sem mattis eros posuere vitae tristique justo congue curabitur consectetur.</p>
+            </div>
+        </div>
+        <p class="p5">Venenatis aliquam sit amet arcu justo in commodo consectetur lacus ac ultrices cras porta dignissim turpis fermentum porttitor aenean scelerisque nunc vel turpis faucibus vestibulum aenean volutpat iaculis nunc, sed accumsan lacus imperdiet eu.</p>
+        <a href="#" class="button">Read more</a>
+        <h2 class="top-1 p3">Culiner</h2>
+        <div class="wrap">
+            <img src="images/page5-img3.jpg" alt="" class="img-border img-indent">
+            <div class="extra-wrap">
+                <p class="color-1 p6">Aenean quis metus lacus, a commodo libero nam lacinia blandit dui vitae malesuada donec</p>
+                <p>Pellentesque scelerisque orci, ac tempor purus vulputate lobortis. Vestibulum porttitor sem mattis eros posuere vitae tristique justo congue curabitur consectetur.</p>
+            </div>
+        </div>
+        <p class="p5">Venenatis aliquam sit amet arcu justo in commodo consectetur lacus ac ultrices cras porta dignissim turpis fermentum porttitor aenean scelerisque nunc vel turpis faucibus vestibulum aenean volutpat iaculis nunc, sed accumsan lacus imperdiet eu.</p>
+        <a href="#" class="button">Read more</a>
+        <h2 class="top-1 p3">Diving & Snorkling</h2>
+        <div class="wrap">
+            <img src="images/page5-img4.jpg" alt="" class="img-border img-indent">
+            <div class="extra-wrap">
+                <p class="color-1 p6">Aenean quis metus lacus, a commodo libero nam lacinia blandit dui vitae malesuada donec</p>
+                <p>Pellentesque scelerisque orci, ac tempor purus vulputate lobortis. Vestibulum porttitor sem mattis eros posuere vitae tristique justo congue curabitur consectetur.</p>
+            </div>
+        </div>
+        <p class="p5">Venenatis aliquam sit amet arcu justo in commodo consectetur lacus ac ultrices cras porta dignissim turpis fermentum porttitor aenean scelerisque nunc vel turpis faucibus vestibulum aenean volutpat iaculis nunc, sed accumsan lacus imperdiet eu.</p>
+        <a href="#" class="button">Read more</a>
+        <h2 class="top-1 p3">Travelling</h2>
+        <div class="wrap">
+            <img src="images/page5-img1.jpg" alt="" class="img-border img-indent">
+            <div class="extra-wrap">
+                <p class="color-1 p6">Aenean quis metus lacus, a commodo libero nam lacinia blandit dui vitae malesuada donec</p>
+                <p>Pellentesque scelerisque orci, ac tempor purus vulputate lobortis. Vestibulum porttitor sem mattis eros posuere vitae tristique justo congue curabitur consectetur.</p>
+            </div>
+        </div>
+        <p class="p5">Venenatis aliquam sit amet arcu justo in commodo consectetur lacus ac ultrices cras porta dignissim turpis fermentum porttitor aenean scelerisque nunc vel turpis faucibus vestibulum aenean volutpat iaculis nunc, sed accumsan lacus imperdiet eu.</p>
+        <a href="#" class="button">Read more</a>
+      </div>
+      <div class="grid_4">
+       
+            </form>
+            <h2 class="p3">Our Contacts</h2>
+                   <dl>
+                <dt class="color-1 p2"><strong>Majapahit Road,<br>Mataram City.</strong></dt>
+                <dd><span>Telephone:</span>(0370) 636 001</dd>
+                <dd><span>E-mail:</span><a href="#" class="link">madinalombok@gmail.com</a></dd>
+            </dl>
+        </div>
+      </div>
+      <div class="clear"></div>
+    </div>  
+</section> 
+@endsection
